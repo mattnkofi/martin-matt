5baf51aa36869d4cc18cb41bda9b228c22222bf8
mattnkofi <justinebolanos1018@gmail.com>
2025-10-05 16:13:52 +0800

    commit

--- app/config/config.php
+++ app/config/config.php
@@ -82,8 +82,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-//$config['base_url'] 				= 'https://dudas-fyra.onrender.com/';
-$config['base_url'] 				= '';
+$config['base_url'] 				= 'https://user-mail-repo-vault.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
